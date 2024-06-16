6013a9c15432e4dd382b7513b260ddb0a7e482d4
amaan848 <amaaniqbal848@icloud.com>
2024-06-16 23:22:27 +0530

    added service structure

---
+++ src/main/java/com/routefare/service/routefare/RouteFareApplication.java
@@ -0,0 +1,13 @@
+package com.routefare.service.routefare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouteFareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouteFareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/routefare/service/routefare/RouteFareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.routefare.service.routefare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouteFareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
